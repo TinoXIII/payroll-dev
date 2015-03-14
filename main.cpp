43369892b11a16eddcc5a22d3f25b6fed5ee119d
Martino Henderson <bolah1313@yahoo.com>
2015-03-14 12:56:29 -0500

    Created the inline class "Person" in the main.cpp file

--- main.cpp
+++ main.cpp
@@ -6,27 +6,64 @@
 
 #include "pr-main.h"
 
-using namespace std;
-
-class People {
+class Person {
 	private:
-		string fname, lname;
+		string firstName;
+		string lastName;
 		int age;
-
 	public:
-		string getFirstName() {return fname;}
-		string getLastName() {return lname;}
-		int getAge() {return age;}
+		string getFirstName(string);
+		string getLastName(string);
+		int getAge(int);
 
-		void setFirstName(string first) {fname = first;}
-		void setLastName(string last) {lname = last;}
-		void setAge(int yrsold) {age = yrsold;}
+		void setFirstName(string);
+		void setLastName(string);
+		void setAge(int);
+	protected:
 };
 
+string Person::getFirstName(string firstName) {
+	return firstName;
+}
+
+string Person::getLastName(string lastName) {
+	return lastName;
+}
+
+int Person::getAge(int age) {
+	return age;
+}
+
+void Person::setFirstName(string fName) {
+	cout << "Enter your first name: ";
+	cin >> fName;
+	firstName = fName;
+}
+
+void Person::setLastName(string lName) {
+	cout << "Enter your last name: ";
+	cin >> lName;
+	lastName = lName;
+}
+
+void Person::setAge(int yrsold) {
+	cout << "Enter your age: ";
+	cin >> yrsold;
+	age = yrsold;
+}
+
 int main() {
+
+	string fName, lName;
+	int yrsold;
+
+	Person newPerson1;
+
 	cout << endl;
 	cout << "\t\tPayroll application\n";
 	cout << endl;
+
+	
 
 	return 0;
 }
